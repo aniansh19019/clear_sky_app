805d8ef38fdfc6a378dc83cf73e9a68d86cc65e6
Aniansh <aniansh@yahoo.com>
2020-07-20 00:18:22 +0530

    Climacell test.py

--- python_test_code/climacell.py
+++ python_test_code/climacell.py
@@ -4,8 +4,10 @@ import math
 
 
 KEY = 'Bk1NEzLKtAp8ZLFAfjnohGxbb0w8ATQn'
-BASE_URL_HOURLY = 'https://api.weatherbit.io/v2.0/forecast/hourly'
+BASE_URL_HOURLY = 'https://api.climacell.co/v3/weather/forecast/hourly'
+START_TIME = 'now'
 
+FIELDS = 'cloud_cover'
 
 LAT = "28.416"
 LONG = "77.840"
@@ -13,15 +15,15 @@ LONG = "77.840"
 
 def get_data():
     data = None
-    request = f'{BASE_URL_HOURLY}?lat={LAT}&lon={LONG}&key={KEY}'
+    request = f'{BASE_URL_HOURLY}?lat={LAT}&lon={LONG}&start_time=now&fields={FIELDS}&unit_system=si&apikey={KEY}'
+    # request = 'https://api.climacell.co/v3/weather/forecast/hourly?lat=28.416&lon=77.84&start_time=now&fields=cloud_cover%2Cwind_gust&apikey=Bk1NEzLKtAp8ZLFAfjnohGxbb0w8ATQn&unit_system=si'
     print(request)
 
     data = urllib.request.urlopen(request)
     data = data.read()
 
     data_dict = json.loads(data)
-    for i in range(len(data_dict['data'])):
-        print(data_dict['data'][i]['clouds'])
+    print(data_dict[0])
 
 
 get_data()
