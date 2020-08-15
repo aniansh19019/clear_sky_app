927ae65588be7669205ac62cf18e2e9adec43e64
Aniansh <aniansh@yahoo.com>
2020-08-15 17:32:33 +0530

    removed crashalytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 // import 'dart:js';
 
 import 'package:flutter/material.dart';
-import 'package:phases/pages/about.dart';
+import 'package:phases/pages/test.dart';
 import 'package:flutter/services.dart';
 import 'package:phases/pages/loading.dart';
 import 'package:phases/tabs.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async
 {
-  Crashlytics.instance.enableInDevMode = false;
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  // Crashlytics.instance.enableInDevMode = false;
+  // FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/',
+    initialRoute: '/test',
     routes: {
       '/' : (context) => Loading(),
       '/tabs' : (context) => TabPage(),
-      '/about': (context) => About(),
+      '/test': (context) => Test(),
     },
 
   ));
